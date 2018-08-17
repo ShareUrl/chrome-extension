cb3a0131dd7f344a1cadc1164354fa7778e1a464
i334502 <vishwanath.kulkarni@sap.com>
2018-08-17 23:23:56 +0530

    added Expiration time

--- scripts/popup.js
+++ scripts/popup.js
@@ -11,7 +11,7 @@ function getDomain(e) {
         domain = o[i];
         break
       }
-      "undefined" == typeof domain && (domain = server)
+    "undefined" == typeof domain && (domain = server)
   }
   return domain
 }
@@ -22,91 +22,124 @@ var content = "",
   downloadable = "",
   popup = "",
   jsons = [];
-  setJsons = [];
+setJsons = [];
 
-  function reloadTabForMe(){
-  chrome.tabs.getSelected(null, function(tab) {
-  var code = 'window.location.reload();';
-  chrome.tabs.executeScript(tab.id, {code: code});
-});
+function reloadTabForMe() {
+  chrome.tabs.getSelected(null, function (tab) {
+    var code = 'window.location.reload();';
+    chrome.tabs.executeScript(tab.id, {
+      code: code
+    });
+  });
 }
 
-function sendDataToServer(jsonData){
+function sendDataToServer(jsonData) {
   $.ajax({
     url: 'https://shareurl-007.appspot.com/createUrl',
     data: JSON.stringify(jsonData),
     type: 'POST',
-    success: function(response) {
-        document.getElementById("inputUrl").value = "https://shareurl.in/"+response;
-        document.getElementById("inputUrl").select();
-        console.log(response);
+    success: function (response) {
+      document.getElementById("inputUrl").value = "https://shareurl.in/" + response;
+      document.getElementById("inputUrl").select();
+      console.log(response);
     },
-    error: function(error) {
+    error: function (error) {
       document.getElementById("inputUrl").value = "an error occered. please try again.";
       console.log(error);
     }
-});
+  });
 }
 
-function setCookieForMe(){
-  for(var i=0;i<setJsons.length;i++){
+function setCookieForMe() {
+  for (var i = 0; i < setJsons.length; i++) {
     var singleCookie = setJsons[i];
     singleCookie.url = "http" + ((singleCookie.secure) ? "s" : "") + "://" + singleCookie.domain + singleCookie.path;
     delete singleCookie["secure"];
     singleCookie["expirationDate"] = parseInt(singleCookie["expirationDate"]);
-     delete singleCookie["expirationDate"];
+    delete singleCookie["expirationDate"];
     console.log(JSON.stringify(setJsons[i]));
     console.log(singleCookie);
-    
-    chrome.cookies.set(setJsons[i],function (cookie){
-        console.log(JSON.stringify(cookie));
-        console.log(chrome.extension.lastError);
-        console.log(chrome.runtime.lastError);
+
+    chrome.cookies.set(setJsons[i], function (cookie) {
+      console.log(JSON.stringify(cookie));
+      console.log(chrome.extension.lastError);
+      console.log(chrome.runtime.lastError);
     });
   }
   sendDataToServer(setJsons);
-  reloadTabForMe(); 
+  reloadTabForMe();
 }
 
-function parseCookieForMe(){
+function parseCookieForMe() {
   var inputString = document.getElementById("jsonInput").value;
-  try{
-      setJsons = JSON.parse(inputString);
-      console.log(setJsons);
-  }catch(e){
+  try {
+    setJsons = JSON.parse(inputString);
+    console.log(setJsons);
+  } catch (e) {
     console.log(e);
     var errorText = document.getElementById("errorText");
-    errorText.style.display="";
+    errorText.style.display = "";
   }
 }
 
-function createShareUrl(){
+function createShareUrl() {
   console.log(setJsons);
+  setExpiryDate();
   sendDataToServer(setJsons);
 }
 
-function copyShareUrl(){
+function setExpiryDate() {
+  Date.prototype.addHours = function (h) {
+    this.setHours(this.getHours() + h);
+    return this;
+  }
+  var exp = 0;
+  if (document.getElementById("neverRadio").checked) {
+    exp = 1;
+  }
+  if (document.getElementById("1weekRadio").checked) {
+    exp = 2;
+  }
+  if (document.getElementById("12hourRadio").checked) {
+    exp = 3;
+  }
+  if(exp==1){
+    return;
+  }
+  val = JSON.parse(setJsons);
+  for (var i; i < val.length; i++) {
+    if (exp == 2) {
+      val[i]["expirationDate"] = new Date().addHour(24 * 7).getTime();
+    }
+    if (exp == 3) {
+      val[i]["expirationDate"] = new Date().addHour(12).getTime();
+    }
+  }
+  setJsons = JSON.stringify(val);
+}
+
+function copyShareUrl() {
   document.getElementById("inputUrl").select();
   document.execCommand("copy");
 }
 
 
-chrome.tabs.getSelected(null, function(e) {
+chrome.tabs.getSelected(null, function (e) {
   domain = getDomain(e.url);
   console.log(domain);
-  chrome.cookies.getAll({}, function(o) {
+  chrome.cookies.getAll({}, function (o) {
 
     for (var t in o) {
       cookie = o[t];
 
       if (-1 != cookie.domain.indexOf(domain)) {
         jsons.push({
-          name : escapeForPre(cookie.name),
+          name: escapeForPre(cookie.name),
           value: escapeForPre(cookie.value),
           domain: escapeForPre(cookie.domain),
-          path : escapeForPre(cookie.path),
-          expirationDate : escapeForPre(cookie.expirationDate ? cookie.expirationDate : "0"),
-          secure : cookie.secure
+          path: escapeForPre(cookie.path),
+          expirationDate: escapeForPre(cookie.expirationDate ? cookie.expirationDate : "0"),
+          secure: cookie.secure
 
         });
 
@@ -118,6 +151,6 @@ chrome.tabs.getSelected(null, function(e) {
     setJsons = content;
     console.log(setJsons);
     document.getElementById("createButton").addEventListener("click", createShareUrl);
-    document.getElementById("copyButton").addEventListener("click", copyShareUrl);    
+    document.getElementById("copyButton").addEventListener("click", copyShareUrl);
   });
 });
